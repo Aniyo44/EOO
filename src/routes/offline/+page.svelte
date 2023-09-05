673f4b5472efc496a1a0b5f91e573cb43bbc1439
aniyo44 <yaf_did@outlook.com>
2023-09-05 21:08:15 +0200

    yes

--- src/routes/offline/+page.svelte
+++ src/routes/offline/+page.svelte
@@ -2,7 +2,7 @@
 let modeChoice:string=""
 let numberChoice:number=1
 let random:number=0
-let lastScreen:boolean=false
+let thridScreen:boolean=false
 let numberScreen:boolean=false
 
 let winner:boolean=false
@@ -10,27 +10,25 @@ let round:number=1
 let score:number=0
 let score2:number=0
 
-console.log("initaliy",random)
 function changeMode(s:string){
     modeChoice=s
     numberScreen=true
 }
 function changeNumber(n:number){
     numberChoice=n
-    lastScreen=true
+    thridScreen=true
     numberScreen=false
     random=Math.floor(Math.random() * 5)+1
-    console.log(random)
     check()
 }
 function next(){
-    lastScreen=false
+    thridScreen=false
     numberScreen=true
+    winner=false
+    round++
 }
 function check(){
-    console.log(random)
     const sum=numberChoice+random
-    console.log(sum)
     if((modeChoice==="even"&& sum % 2 === 0)||(modeChoice==="odd"&& sum % 2 !== 0)){
         winner=true
         score ++
@@ -39,16 +37,28 @@ function check(){
     }
    
 }
+function restart(){
+ modeChoice=""
+ numberChoice=1
+ random=0
+ thridScreen=false
+ numberScreen=false
+ winner=false
+ round=1
+ score=0
+ score2=0
 
+}
 
 </script>
 <div class="flex justify-center items-center flex-col">
-<p>choose</p>
+    <!-- the last screen the one with won or lost the game-->
+
 {#if score ===3 || score2 ===3}
 <p>last round </p>
 <p>{modeChoice}</p>
-      <p>{numberChoice}</p>
-      <p>{random}</p>
+<p>{numberChoice}</p>
+<p>{random}</p>
 <p>game over</p>
 <p>blue :{score} vs red: {score2}</p>
 {#if score===3}
@@ -56,33 +66,58 @@ function check(){
 {:else}
 <p>red side win</p>
 {/if}
+<button on:click={restart}>Restart</button>
+<!-- thrid screen the one with the lost or won the round -->
 {:else}
-{#if lastScreen}
+{#if thridScreen}
+<p>Round:{round}</p>
     {#if winner}
-      <p>You're a Winner!</p>
-      <p>{modeChoice}</p>
+    <p>{modeChoice}</p>
 
-      <p>{numberChoice}</p>
-      <p>{random}</p>
+      <p>You're a Winner!</p>
+
+
+      <div class="flex justify-center flex-row space-x-5"> 
+        <div>
+            <p>blue  side</p><p>{numberChoice}</p></div>
+        <div>
+            <p>red side</p>
+            <p>{random}</p></div>
+      </div>
+      {#if score>0||score2>0}
+      <p>blue:{score} vs red:{score2}</p>
+      {/if}
       <button on:click={next}>go next round</button>
 
     {:else}
-      <p>Sorry, You Lost.</p>
-      <p>{modeChoice}</p>
+    <p>{modeChoice}</p>
 
-      <p>{numberChoice}</p>
-      <p>{random}</p>
+      <p>Sorry, You Lost.</p>
+
+      <div class="flex justify-center flex-row"> 
+        <div>
+            <p>blue  side</p><p>{numberChoice}</p></div>
+        <div>
+            <p>red side</p>
+            <p>{random}</p></div>
+      </div>
+      {#if score>0||score2>0}
+      <p>blue:{score} vs red:{score2}</p>
+      {/if}
 
       <button on:click={next}>go next round</button>
 
 
     {/if}
+    <!-- second screen the one with numbers and we keep looping over it -->
   {:else}
  {#if modeChoice.length >0 && numberScreen}   
  <p>{modeChoice}</p>
  {#if score>0||score2>0}
  <p>blue:{score} vs red:{score2}</p>
  {/if}
+ <p>Round:{round}</p>
+
  <div class="flex justify-center flex-row space-x-2">
 <button on:click={()=>changeNumber(1)}>1</button>
 <button on:click={()=>changeNumber(2)}>2</button>
@@ -91,6 +126,8 @@ function check(){
 <button on:click={()=>changeNumber(5)}>5</button>
  </div>
 {:else}
+<!-- the first screen where you choose even or odd it pop up once -->
+<p>choose</p>
  <div class="flex flex-row space-x-2">
 <button on:click={()=>changeMode("even")} >Even</button>
 <button on:click={()=>changeMode("odd")} >Odd</button>
