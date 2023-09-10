ffcd9824bce631f30499b1d2684bf282640d8d49
aniyo44 <yaf_did@outlook.com>
2023-09-10 16:23:25 +0200

    m

--- src/lib/install.svelte
+++ src/lib/install.svelte
@@ -1,39 +1,26 @@
-<!-- InstallPrompt.svelte -->
 <script lang="ts">
+    import { onMount } from 'svelte';
+  
     let deferredPrompt:any;
-
-    // Listen for the beforeinstallprompt event
-    window.addEventListener('beforeinstallprompt', (e) => {
-        e.preventDefault();
+  
+    onMount(() => {
+      window.addEventListener('beforeinstallprompt', (e) => {
         deferredPrompt = e;
+      });
     });
-
-    // Show the install button when deferredPrompt is available
-    $: showInstallButton = !!deferredPrompt;
-
-    // Handle install button click
-    function installApp() {
-        if (deferredPrompt) {
-            deferredPrompt.prompt();
-            deferredPrompt.userChoice.then((choiceResult:any) => {
-                if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the install prompt');
-                } else {
-                    console.log('User dismissed the install prompt');
-                }
-                deferredPrompt = null;
-            });
+  
+    const installApp = async () => {
+      if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+          deferredPrompt = null;
         }
-    }
-
-    // Hide the install button when the PWA is installed
-    window.addEventListener('appinstalled', () => {
-        deferredPrompt = null;
-    });
-</script>
-
-<div>
-    {#if showInstallButton}
-        <button on:click={installApp}>Install App</button>
-    {/if}
-</div>
+      }
+    };
+  </script>
+  
+  <div>
+    <button on:click={installApp}>Install App</button>
+  </div>
+  
