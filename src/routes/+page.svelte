6b5c79a57ec78ce27b37a423f7eb4955df77e8ec
aniyo44 <yaf_did@outlook.com>
2023-09-10 17:48:38 +0200

    m

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,35 +9,40 @@ function reveal(){
 }
 
 </script>
-<div class="flex justify-center items-center flex-col mt-10">
-<h1>Even or Odd</h1>
+<div class="flex  justify-center mt-28"><h1>Even or Odd</h1>
+</div>
+<div class="flex justify-center items-center flex-col mt-20">
 {#if show}
-<a href="/offline" class="text-white border-2 
+<div class="flex flex-row space-x-2">
+<a href="/offline" class="text-white text-3xl border-2 
 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))]
  from-amber-200 via-violet-600 to-sky-900 border-gray-500
-  p-2 rounded-full">offline</a>
+  p-3 rounded-full">offline</a>
 <a href="/online" class="text-white border-2 
 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))]
  from-amber-200 via-violet-600 to-sky-900 border-gray-500
-  p-2 rounded-full">online</a>
+  p-3 text-3xl rounded-full">online</a>
+</div>
 {:else}
-<button on:click={reveal}  class="text-white border-2 
+<button on:click={reveal}  class="text-5xl text-white border-2 
 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
  from-gray-300 via-fuchsia-600 to-orange-600 border-gray-500
-  p-2 rounded-full">PLAY</button>
-<a href="/records" style="background: var(--custom-gradient);" class="text-white border-2 
+  p-3 rounded-full">PLAY</button>
+<a href="/records" style="background: var(--custom-gradient);" class="mt-10 text-2xl
+p-2 text-white
+ border-2 
  border-gray-500
   p-1 rounded-full">records</a>
 {/if}
-<img src={full_left} class="w-40 h-40" />
 
 
 
 
 </div>
-<div class="absolute bottom-16  right-1/2 translate-x-1/2 ">
+<div class="absolute bottom-1  right-1/2 translate-x-1/2 ">
     <div>
       <Install/>
+      
 </div>
 </div>
 
